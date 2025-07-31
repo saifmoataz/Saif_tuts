5cf99cd653b3e884eeb3654efa7b12550e26bf11
Saif moataz osam <saifmoatazosamelsayed@gmail.com>
2025-07-31 18:52:45 +0300

    initail commit

--- next.config.js
+++ next.config.js
@@ -1,36 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/Saif_tuts',
+  assetPrefix: '/Saif_tuts/',
   images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
